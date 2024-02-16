be2968356c1404e813095abfa05adabd1bc080b4
Kavyapuli <pulikavya10@gmail.com>
2024-02-16 10:00:08 +0530

    finalcommit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConApp;
+
+Console.WriteLine("Welcome to Number System");
+Console.WriteLine("*****OddNumber***");
+NumberSystem.oddNum();
+Console.WriteLine("*****EvenNumbers***");
+NumberSystem.evenNum();
